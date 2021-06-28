865d73e145a8a45b93555225775c8c5763956b0c
LuisDanilo <luis.danilo.95@hotmail.com>
2021-06-28 15:34:31 -0500

    dts bundle minimal setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
 
 export default [
     {
@@ -20,5 +21,10 @@ export default [
             },
         ],
         plugins: [typescript()]
+    },
+    {
+        input: './dist/dts/index.d.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        plugins: [dts()]
     }
 ]
