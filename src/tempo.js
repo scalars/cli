2f4fad986b19f30e048d3793c21e48375c304d15
LuisDanilo <luis.danilo.95@hotmail.com>
2021-07-07 16:34:49 -0500

    returned object on query response

--- src/tempo.js
+++ src/tempo.js
@@ -1,4 +1,14 @@
-import { ScalarsClient } from '../dist/index'
+const { ScalarClient } = require('../dist/index')
 
 const client = new ScalarsClient()
-client.
+
+const profileSelect = {
+    id: true,
+    description: true
+}
+
+client.query
+    .profile(profileSelect, {
+        email: 'luis@madrov.com'
+    })
+    .then( res => console.log( res ) )
