347f78a97ccd14e09525526070b752ffda538f96
LuisDanilo <luis.danilo.95@hotmail.com>
2021-07-29 17:53:12 -0500

    sync command refactor

--- src/scripts/sync.ts
+++ src/scripts/sync.ts
@@ -2,9 +2,12 @@ import { introspect } from '../utils/introspect'
 import { config } from 'dotenv'
 import { join } from 'path'
 
-let endpoint: string | undefined
-let clientId: string | undefined
+let endpoint: string | undefined = process.env.SCALARS_ENDPOINT
+let clientId: string | undefined = process.env.SCALARS_CLIENT_ID
 
+/**
+ * Function that starts introspection
+ */
 const sync = () => {
     introspect( {
         endpoint: endpoint as string,
@@ -15,30 +18,34 @@ const sync = () => {
     } )
 }
 
-let errors: boolean = false
-const production: boolean = process.env.NODE_ENV === 'production'
-const envPath: string = join( process.cwd() , `${ production ? '.env' : '.env.dev'}` )
-const { error } = config( { path: envPath } )
-errors = !!error
-if ( !errors ) {
-    endpoint = process.env.SCALARS_API || process.env.SCALARS_ENDPOINT
-    clientId = process.env.SCALARS_CLIENT_ID
-    errors = !endpoint && !clientId
-    if ( !errors ) {
-        const arg: string = process.argv[2]
-        if ( arg && /sync/g.test( arg ) ) {
-            sync()
+const arg = process.argv[2]
+if ( arg && /^sync$/g.test( arg ) ) {
+    let err: boolean = !endpoint && !clientId
+    if ( err ) {
+        // Trying to load endpoint and client id through dotenv
+        const production: boolean = process.env.NODE_ENV === 'production'
+        const envPath: string = join( process.cwd() , `${ production ? '.env.prod' : '.env.dev'}` )
+        const { error } = config( { path: envPath } )
+        err = !!error
+        if ( !err ) {
+            // Environment variables set
+            endpoint = process.env.SCALARS_ENDPOINT
+            clientId = process.env.SCALARS_CLIENT_ID
+            err = !endpoint && !clientId
+            if ( !err ) {
+                sync()
+            }
+            else {
+                console.log( `Error!\n\tMake sure you specified your scalars endpoint as SCALARS_ENDPOINT and\n\tyour client id as SCALARS_CLIENT_ID at ${envPath}.` )
+            }
         }
         else {
-            console.log( `Error!\n\t${arg ? `${arg} is not an option` : `must specify an argument for scalars command`}` )
+            console.error( `Error!\n\tFailed to load environment variables from ${envPath}` )
         }
+    } else {
+        sync()
     }
-    else {
-        console.log( `Error!\n\tMake sure you specified your scalars API endpoint as SCALARS_API or SCALARS_ENDPOINT and\n\tyour client id as SCALARS_CLIENT_ID at ${envPath}.` )
-    }
-}
-else {
-    console.error( `Error!\n\tFailed to load environment variables from ${envPath}` )
-}
 
-
+} else {
+    console.log( `Error!\n\t${arg ? `${arg} is not an option` : `must specify an argument for scalars command`}` )
+}
