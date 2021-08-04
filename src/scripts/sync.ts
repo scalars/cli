06ec76b8200bc1cee460c4cefc3b478e7cf77e7b
LuisDanilo <luis.danilo.95@hotmail.com>
2021-08-04 17:28:09 -0500

    inquirer added

--- src/scripts/sync.ts
+++ src/scripts/sync.ts
@@ -2,9 +2,34 @@ import { introspect } from '../utils/introspect'
 import { config } from 'dotenv'
 import { join } from 'path'
 import { usage } from 'yargs'
+import inquirer from 'inquirer'
 
+// Trying to set endpoint and clientId by envoronment variables
 let endpoint: string | null = process.env.SCALARS_ENDPOINT | null
 let clientId: string | null = process.env.SCALARS_CLIENT_ID | null
+
+/**
+ * Function that starts introspection
+ */
+const sync = () => {
+    inquirer
+        .prompt( [
+            // Questions
+        ] )
+        .then( ( answers ) => {
+            // Use feedback
+        } )
+        .catch( ( error ) => {
+            console.log( error )
+        } )
+    introspect( {
+        endpoint: endpoint as string,
+        clientId: clientId as string
+    } ).then( () => {
+        console.clear()
+        console.log( `Introspection completed!!\n\tThanks for using scalars client!\n\tOn your code you need to create an instance of the client like this:\n\t\tconst client = new ScalarsClient()\n\tHappy codding!!\n\tYour friendly neighbors Luis Danilo JG and Madrov team ❤️` )
+    } )
+}
 
 export const argv = usage( '$0 command' )
     .command( 'sync', 'Sync api connection scalars', ( yargs ) => {
@@ -17,16 +42,12 @@ export const argv = usage( '$0 command' )
             const { error } = config( { path: envPath } )
             err = !!error
             if ( !err ) {
-                // dotenv has loaded .env.dev file
+                // dotenv has loaded .env.* file
                 endpoint = process.env.SCALARS_ENDPOINT
                 clientId = process.env.SCALARS_CLIENT_ID
                 err = !endpoint && !clientId
-                if ( !err ) {
-                    sync()
-                }
-                else {
-                    console.log( `Error!\n\tMake sure you specified your scalars endpoint as SCALARS_ENDPOINT and\n\tyour client id as SCALARS_CLIENT_ID at ${envPath}.` )
-                }
+                if ( !err ) sync()
+                else console.log( `Error!\n\tMake sure you specified your scalars endpoint as SCALARS_ENDPOINT and\n\tyour client id as SCALARS_CLIENT_ID at ${envPath}.` )
             }
             else {
                 console.error( `Error!\n\tFailed to load environment variables from ${envPath}` )
@@ -39,17 +60,4 @@ export const argv = usage( '$0 command' )
     .demand( 1, 'You must provide a valid scalars command' )
     .alias( 'h', 'help' )
     .argv
-
-/**
- * Function that starts introspection
- */
-const sync = () => {
-    introspect( {
-        endpoint: endpoint as string,
-        clientId: clientId as string
-    } ).then( () => {
-        console.clear()
-        console.log( `Introspection completed!!\n\tThanks for using scalars client!\n\tOn your code you need to create an instance of the client like this:\n\t\tconst client = new ScalarsClient()\n\tHappy codding!!\n\tYour friendly neighbors Luis Danilo JG and Madrov team ❤️` )
-    } )
-}
 
