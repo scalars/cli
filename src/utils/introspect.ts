0b9466b5f19cc7f0be14538caa61a0a9904e744a
LuisDanilo <luis.danilo.95@hotmail.com>
2021-08-20 14:46:12 -0500

    minor fix

--- src/utils/introspect.ts
+++ src/utils/introspect.ts
@@ -40,8 +40,8 @@ const generateTypedSchema = async ( scalarsEndpoint: string ): Promise<string> =
  * @param config Client configuration (endpoint and client id)
  */
 const updateScalarsClient = async ( operations: Record<string, any>, config: ScalarsClientConfig ): Promise<void> => {
-    const outputPath = __dirname //config.clientPath
-    // !existsSync( outputPath ) && !!mkdirSync( outputPath, { recursive: true } )
+    const outputPath = config.clientPath
+    !existsSync( outputPath ) && !!mkdirSync( outputPath, { recursive: true } )
     // const canPerformSoftIntrospection: boolean = config.soft
     //     && existsSync( resolve( outputPath, 'index.ts' ) )
     // if ( !canPerformSoftIntrospection ) {
@@ -89,7 +89,7 @@ const updateScalarsClient = async ( operations: Record<string, any>, config: Sca
         join( __dirname, 'templates', 'ScalarsClient.mustache' )
     ).toString()
     writeFileSync(
-        resolve( outputPath, 'index.ts' ),
+        resolve( outputPath, outputPath !== __dirname ? 'ScalarsClient.ts' : 'index.ts' ),
         render( defaultServicesTemplate, {
             operations,
             schemaTypes: schemaTypes,
@@ -101,22 +101,24 @@ const updateScalarsClient = async ( operations: Record<string, any>, config: Sca
     // -------------------------------------------------------------------------
 
     // -------------------------------------------------------------------------
-    try {
-        compile( [resolve( __dirname, 'index.ts' )], {
-            declaration: true,
-            emitDeclarationOnly: false,
-            target: 'es2019',
-            module: 'commonjs',
-            moduleResolution: 'node',
-            strict: true,
-            esModuleInterop: true,
-            skipLibCheck: true,
-            forceConsistentCasingInFileNames: true,
-            outDir: __dirname,
-        } )
-    }
-    catch ( e ) {
-        console.log( e )
+    if ( outputPath === __dirname ) {
+        try {
+            compile( [resolve( __dirname, 'index.ts' )], {
+                declaration: true,
+                emitDeclarationOnly: false,
+                target: 'es2019',
+                module: 'commonjs',
+                moduleResolution: 'node',
+                strict: true,
+                esModuleInterop: true,
+                skipLibCheck: true,
+                forceConsistentCasingInFileNames: true,
+                outDir: __dirname,
+            } )
+        }
+        catch ( e ) {
+            console.log( e )
+        }
     }
 }
 
